c536920b8807e83a86cd1b0f825dd649efa24519
Banana <alilamas@outlook.com>
2021-03-05 11:10:27 +0800

    fix:  新增_02_design文件夹

--- src/router/modules/webpPage.js
+++ src/router/modules/webpPage.js
@@ -1,21 +1,21 @@
 export default [{
   path: '/webpPageHome',
   name: 'webpPageHome',
-  component: () => import('@/views/webpPage/home'),
+  component: () => import('@/views/_02_design/home'),
   meta: {
     title: '页面设计'
   },
   children: [{
     path: '/webpPage',
     name: 'webpPage',
-    component: () => import('@/views/webpPage'),
+    component: () => import('@/views/_02_design'),
     meta: {
       title: '页面设计'
     }
   }, {
     path: '/webpPage/details/:id',
     name: 'webpPageDetails',
-    component: () => import('@/views/webpPage/details'),
+    component: () => import('@/views/_02_design/details'),
     meta: {
       title: '页面设计'
     }
