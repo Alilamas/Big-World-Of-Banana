0fdf97d6989e0119df348a8efd4cebef49b082d6
Banana <alilamas@outlook.com>
2021-03-05 10:59:51 +0800

    fix:  新增_01_basic文件夹

--- src/router/modules/graphical.js
+++ src/router/modules/graphical.js
@@ -1,7 +1,7 @@
 export default [{
   path: '/graphical',
   name: 'graphical',
-  component: () => import('@/views/_01_graphical/hexagon.vue'),
+  component: () => import('@/views/_01_basic/graphical/hexagon.vue'),
   meta: {
     title: '六边形的多种做法'
   }
