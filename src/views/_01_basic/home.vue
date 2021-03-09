b06389571a739328ec892d410d1dc3010b824dc1
banana <alilamas@outlook.com>
2021-03-09 21:51:17 +0800

    修改冲突

--- src/views/_01_basic/home.vue
+++ src/views/_01_basic/home.vue
@@ -1,11 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <div id="basicHome">
-    <nav>
-=======
   <div class="basicHome">
     <!-- <nav>
->>>>>>> 0b3283687cacf4b0622a1dbaea90c4f19ee702f4
       <ul>
         <li @click="pushRouter('graphical')">图形graphical</li>
       </ul>
@@ -14,23 +9,6 @@
   </div>
 </template>
 <style lang="scss" scoped>
-<<<<<<< HEAD
-#basicHome {
-  background: linear-gradient(to right,#acacac, #eaeaea);
-  width: 100%;
-  min-height: 100vh;
-}
-nav {
-  position: fixed;
-  left: 0;
-  top: 0;
-  ul {
-    display: flex;
-    li {
-    }
-  }
-}
-=======
 // #basicHome {
 // }
 // nav {
@@ -43,5 +21,4 @@ nav {
 //     }
 //   }
 // }
->>>>>>> 0b3283687cacf4b0622a1dbaea90c4f19ee702f4
 </style>
