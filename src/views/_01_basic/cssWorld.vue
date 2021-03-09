591bc34792146a840b5ed06dabbcd7685e7aff1d
banana <alilamas@outlook.com>
2021-03-09 21:58:33 +0800

    新建cssWorld文件

---
+++ src/views/_01_basic/cssWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="cssWorld">
+    <h1>概述</h1>
+    <p>css看中的是特性间的<b>相互联系</b>和<b>具象能力</b></p>
+    <p>CSS全称 Cascading Style Sheets(层叠样式表)</p>
+    <p><b>CSS世界的诞生就是为图文信息展示服务的 </b></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <b></b>
+    <i></i>
+
+
+    <p></p>
+    <b></b>
+    <i></i>
+
+
+    <p></p>
+    <b></b>
+    <i></i>
+  </div>
+</template>
+<style lang="scss" scoped>
+#cssWorld {
+  width: 100vw;
+  height: 100vh;
+  background: #393535;
+}
+</style>
--- src/views/_01_basic/index.vue
+++ src/views/_01_basic/index.vue
@@ -1,27 +1,5 @@
 <template>
-  <div id="basic">
-    <h1>概述</h1>
-    <p>css看中的是特性间的<b>相互联系</b>和<b>具象能力</b></p>
-    <p>CSS全称 Cascading Style Sheets(层叠样式表)</p>
-    <p><b>CSS世界的诞生就是为图文信息展示服务的 </b></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <b></b>
-    <i></i>
-
-
-    <p></p>
-    <b></b>
-    <i></i>
-
-
-    <p></p>
-    <b></b>
-    <i></i>
+  <div id="basicIndex">
     <div class="magic">
       <h5>HTML<b>&</b>CSS</h5>
       <P>besign and bulid website</P>
@@ -38,7 +16,7 @@ $width: 200px;
 $height: 40px;
 $margin: ($width - $height/2)*$sqrt2;
 
-#basic {
+#basicIndex {
   width: 100vw;
   height: 100vh;
   background: #393535;
