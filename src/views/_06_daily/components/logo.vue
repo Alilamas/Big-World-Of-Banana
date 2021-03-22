761b0f0d80ecc2105ba665ec429fc6bfc55c4eb9
Banana <alilamas@outlook.com>
2021-03-22 18:04:41 +0800

    fix:  提取logo组件

---
+++ src/views/_06_daily/components/logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="daily_logo">
+    <span class="dl1">lo</span><span class="dl2">ve</span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.daily_logo {
+  padding-top: 10px;
+  font-size: 30px;
+  color: #ffffff;
+  font-family: "Pacifico";
+  transform: rotateZ(-20deg);
+  span {
+    display: inline-block;
+  }
+  .dl1 {
+    position: relative;
+    top: -4px;
+    left: 10px;
+    transform: rotateZ(90deg);
+  }
+}
+</style>
--- src/views/_06_daily/home.vue
+++ src/views/_06_daily/home.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <nav>
-      <div class="daily_logo"><span class="dl1">lo</span><span class='dl2'>ve</span></div>
+      <Logo />
       <ul>
         <li></li>
       </ul>
     </nav>
     <router-view class="daliy_view"></router-view>
-    <banana/>
+    <banana />
   </div>
 </template>
 
 <script>
+import Logo from "./components/logo";
 export default {
+  components: {
+    Logo,
+  },
   data() {
     return {};
   },
@@ -28,28 +32,12 @@ nav {
   position: fixed;
   left: 0;
   top: 0;
-  z-index:100;
+  z-index: 100;
   display: flex;
-  justify-content:left;
-  .daily_logo {
-    padding-top: 10px;
-    font-size: 30px;
-    color: #ffffff;
-    font-family: "Pacifico";
-      transform: rotateZ(-20deg);
-    span {
-      display:inline-block;
-    }
-    .dl1 {
-      position: relative;
-      top: -4px;
-      left: 10px;
-      transform: rotateZ(90deg);
-    }
-  }
+  justify-content: left;
   ul {
-  display: flex;
-  justify-content:left;
+    display: flex;
+    justify-content: left;
   }
 }
 </style>
