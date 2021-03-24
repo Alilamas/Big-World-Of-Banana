bc225810cc23688dc4283cb8837a8adb414b90d7
Banana <alilamas@outlook.com>
2021-03-24 18:27:07 +0800

    transiton-timing-function -> steps()

--- src/views/_01_basic/transition.vue
+++ src/views/_01_basic/transition.vue
@@ -6,9 +6,13 @@
     <p>分别是：过渡css属性，所需时间，动画函数，延迟时间</p>
     <div class="test">
       <h5>测试transiton-property</h5>
-      <p>很多自己去看吧，主要是能有中点值，可以过渡，比如颜色、长度；但是如font-family这些就不行；</p>
+      <p>
+        很多自己去看吧，主要是能有中点值，可以过渡，比如颜色、长度；但是如font-family这些就不行；
+      </p>
       <h5>测试transiton-duration</h5>
-      <p>s和ms都可以，就是简单的一套动画完成所需的时间；<br/>时间为0的话，就是没有过度，直接看到结果；<br/>时间也是可以设置多个的，逗号分隔，与-property的对应；</p>
+      <p>
+        s和ms都可以，就是简单的一套动画完成所需的时间；<br />时间为0的话，就是没有过度，直接看到结果；<br />时间也是可以设置多个的，逗号分隔，与-property的对应；
+      </p>
       <h5>测试transiton-timing-function</h5>
       <h5>部分一：</h5>
       <p>ease：先快到慢；</p>
@@ -24,7 +28,9 @@
       <p>工具网址 -> http://cubic-bezier.com</p>
       <h5>部分三来了来了！！！</h5>
       <p>step()函数</p>
-      
+      <div class="test1">
+        <div class="test1__1"></div>
+      </div>
       <h5>测试transiton-delay</h5>
       <p>同transiton-duration</p>
     </div>
@@ -46,13 +52,14 @@ h3,
 h4,
 h5 {
   line-height: 50px;
-  font-size:24px;
+  font-size: 24px;
 }
 p,
 span {
   line-height: 25px;
 }
 .transition {
+  padding-bottom: 200px;
   min-height: 100vh;
   background-color: #22669988;
   background-image: linear-gradient(white 2px, transparent 2px),
@@ -62,6 +69,24 @@ span {
   background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
   background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
 }
-.test {
+.test1 {
+  margin-left: 20px;
+  &__1 {
+    width: 100px;
+    height: 100px;
+    box-sizing: border-box;
+    border: 1px solid #000;
+    background: #00c1de88;
+    animation: 2s test1 steps(4,start);
+    // animation: 2s test1 cubic-bezier(1,1.57,0,.74);
+    @keyframes test1 {
+      0%{
+        transform: translateX(0px);
+      }
+      100%{
+        transform: translateX(80px);
+      }
+    }
+  }
 }
 </style>
