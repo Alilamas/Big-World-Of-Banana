fc20e30238f5c4be5a3a998f9be3fed5c889835e
Banana <alilamas@outlook.com>
2021-03-24 17:57:12 +0800

    测试transiton-timing-function > 贝塞尔曲线

--- src/views/_01_basic/transition.vue
+++ src/views/_01_basic/transition.vue
@@ -10,13 +10,23 @@
       <h5>测试transiton-duration</h5>
       <p>s和ms都可以，就是简单的一套动画完成所需的时间；<br/>时间为0的话，就是没有过度，直接看到结果；<br/>时间也是可以设置多个的，逗号分隔，与-property的对应；</p>
       <h5>测试transiton-timing-function</h5>
+      <h5>部分一：</h5>
       <p>ease：先快到慢；</p>
       <p>linear：均速；</p>
       <p>ease-in：先比正常慢，然后越来越快；</p>
       <p>ease-out：先比正常快，然后越来越慢；</p>
       <p>ease-out-in：先加速后减速；</p>
+      <p><b>详见454页</b></p>
+      <h5>部分二：来了来了！！！</h5>
+      <h5>贝塞尔曲线！bezier！</h5>
+      <h5>函数名：cubic-bezier</h5>
+      <p>意思为三次贝塞尔曲</p>
+      <p>工具网址 -> http://cubic-bezier.com</p>
+      <h5>部分三来了来了！！！</h5>
+      <p>step()函数</p>
+      
       <h5>测试transiton-delay</h5>
-      <p></p>
+      <p>同transiton-duration</p>
     </div>
   </div>
 </template>
