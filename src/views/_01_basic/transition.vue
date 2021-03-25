a571c068f48fafb3a8d6cb59ad2d07ebf11f554e
Banana <alilamas@outlook.com>
2021-03-25 10:32:03 +0800

    硬件加速

--- src/views/_01_basic/transition.vue
+++ src/views/_01_basic/transition.vue
@@ -25,7 +25,7 @@
       <h5>贝塞尔曲线！bezier！</h5>
       <h5>函数名：cubic-bezier</h5>
       <p>意思为三次贝塞尔曲</p>
-      <p>工具网址 -> http://cubic-bezier.com </p>
+      <p>工具网址 -> http://cubic-bezier.com</p>
       <h5>部分三来了来了！！！</h5>
       <p>step()函数</p>
       <div class="test1">
@@ -33,6 +33,23 @@
       </div>
       <h5>测试transiton-delay</h5>
       <p>同transiton-duration</p>
+      <h5>CSS3触发 →</h5>
+      <h5>通过伪元素触发</h5>
+      <p>:active；:focus；:checked；:hover；</p>
+      <h5>通过媒体查询触发</h5>
+      <p>@media only screen and(max-width:960px)</p>
+      <p><b>可以尝试看看背景渐变</b></p>
+      <h5>通过JS触发</h5>
+      <h5>通过媒体查询触发</h5>
+      <p>大多是说值为 em 的属性；</p>
+      <div class="test2">
+        <h5>测试几乎无限延迟的过渡</h5>
+        <div class="test2__1"></div>
+      </div>
+      <div class="test3">
+        <h5>测试硬件加速</h5>
+        <div class="test3__1"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -61,13 +78,21 @@ span {
 .transition {
   padding-bottom: 200px;
   min-height: 100vh;
-  background-color: #22669988;
+  background-color: green;
+  transition: background-color 0.5s linear;
   background-image: linear-gradient(white 2px, transparent 2px),
     linear-gradient(90deg, white 2px, transparent 2px),
     linear-gradient(#ffffff33 1px, transparent 1px),
     linear-gradient(90deg, #ffffff33 1px, transparent 1px);
   background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
   background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+  @media screen and (max-width: 1260px) {
+    background-color: pink;
+  }
+  @media screen and (max-width: 800px) {
+    transition: background-color 2s linear;
+    background-color: #22669988;
+  }
 }
 .test1 {
   margin-left: 20px;
@@ -77,15 +102,47 @@ span {
     box-sizing: border-box;
     border: 1px solid #000;
     background: #00c1de88;
-    animation: 2s test1 steps(4,start);
+    animation: 2s test1 steps(4, start) infinite;
     // animation: 2s test1 cubic-bezier(1,1.57,0,.74);
     @keyframes test1 {
-      0%{
+      0% {
         transform: translateX(0px);
       }
-      100%{
+      100% {
         transform: translateX(80px);
       }
+    }
+  }
+}
+.test2 {
+  margin-left: 20px;
+  &__1 {
+    width: 100px;
+    height: 100px;
+    box-sizing: border-box;
+    border: 1px solid #000;
+    background: #00c1de88;
+    transition: all 999999s ease-out;
+  }
+
+  &:hover {
+    .test2__1 {
+      transform: translateX(100px);
+      transition: all 2s;
+    }
+  }
+}
+.test3 {
+  margin-left: 20px;
+  &__1 {
+    width: 300px;
+    height: 300px;
+    background: #00c1de;
+    transition: all 2s ease;
+    transform:translate(0);//用来gui加速的
+    &:hover {
+      width: 100px;
+      height: 200px;
     }
   }
 }
