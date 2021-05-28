a394098664dc562bb96aada1c462f20c05ebc863
Banana <alilamas@outlook.com>
2021-05-28 18:05:30 +0800

    新增vueRuler文件

---
+++ src/views/_01_basic/basic/vueRuler.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="vueRuler">
+		<h3>vue 开发命名规范</h3>
+		<div class="bananMD">
+			<h5></h5>
+			<p class="title"></p>
+			<p class="content"></p>
+		</div>
+	</div>
+</template>
